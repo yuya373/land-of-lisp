775a78be83b756344d67c05d5e28b16a62bc893e
Yuya Minami <yuya373@me.com>
2015-07-26 01:22:34 +0900

    add manager

---
+++ manager.lisp
@@ -0,0 +1,9 @@
+(defun add-widget (database widget)
+  (cons widget database))
+
+(defparameter *database* nil)
+
+(defun main-loop ()
+  (loop (princ "Please enter the name of a new widget: ")
+        (setf *database* (add-widget *database* (read)))
+        (format t "The database contains the following ~a~%" *database*)))
