7e2d724834767c440ed90ec9adf342156d5d2ea8
Yuya Minami <yuya373@me.com>
2015-08-02 18:04:44 +0900

    add svg

---
+++ svg.lisp
@@ -0,0 +1,72 @@
+(load "macros")
+
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+          (format t " ~a=\"~a\"" (string-downcase (car att)) (cdr att)))
+        alst)
+  (princ #\>))
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                           (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
+
+(defmacro html (&body body)
+  `(tag html () ,@body))
+
+(defmacro body (&body body)
+  `(tag body () ,@body))
+
+;; (html (body (princ "Hello World!")))
+
+(defmacro svg (width height &body body)
+  `(tag svg (xmlns "http://www.w3.org/2000/svg"
+                   "xmlns:xlink" "http://www.w3.org/1999/xlink"
+                   height ,height
+                   width ,width)
+     ,@body))
+
+(defun brightness (col amt)
+  (mapcar (lambda (x)
+            (min 255 (max 0 (+ x amt))))
+          col))
+
+(defun svg-style (color)
+  (format nil
+          "~{fill:rgb(~a,~a,~a);stroke:rgb(~a,~a,~a)~}"
+          (append color
+                  (brightness color -100))))
+
+(defun circle (center radius color)
+  (tag circle (cx (car center)
+                  cy (cdr center)
+                  r radius
+                  style (svg-style color))))
+
+;; (svg 150 150
+;;   (circle '(50 . 50) 50 '(255 0 0))
+;;   (circle '(100 . 100) 50 '(0 0 255)))
+
+(defun polygon (points color)
+  (tag polygon (points (format nil
+                               "~{~a,~a ~}"
+                               (mapcan (lambda (tp)
+                                         (list (car tp) (cdr tp)))
+                                       points))
+                       style (svg-style color))))
+
+(defun random-walk (value length)
+  (unless (zerop length)
+    (cons value
+          (random-walk (if (zerop (random 2))
+                           (1- value)
+                           (1+ value))
+                       (1- length)))))
